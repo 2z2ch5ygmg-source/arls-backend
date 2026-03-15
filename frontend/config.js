37ab0fdfb7dd1dc5276e72346ad00bdf910f6c17
mark <mark@local.dev>
2026-03-15 09:36:30 +0900

    chore: refresh frontend bundle cache

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.ENV_API_BASE = window.ENV_API_BASE || 'https://rg-arls-backend.azurewebsites.net';
-window.ENV_BUILD_ID = window.ENV_BUILD_ID || '1773556500';
+window.ENV_BUILD_ID = window.ENV_BUILD_ID || '1773534899';
 window.ENV_GOOGLE_MAPS_API_KEY = window.ENV_GOOGLE_MAPS_API_KEY || '';
 
 if (typeof window.__RG_ARLS_HANDLE_TAB_CLICK__ !== 'function') {
--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'rg-arls-sw-v1773305601';
+const SW_VERSION = 'rg-arls-sw-v1773534899';
 const SHELL_CACHE = `${SW_VERSION}-shell`;
 const STATIC_CACHE = `${SW_VERSION}-static`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, STATIC_CACHE]);
